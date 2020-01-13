6ef5629ed330573fc7e4bc3541ca1160c4ff2cfd
Radosław Grzymała <radoslaw.grzymala@sensilabs.pl>
2020-01-13 16:03:06 +0100

    search

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
 import About from '../views/About.vue';
 import Search from '../views/Search.vue';
 
@@ -9,18 +8,13 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home,
+    name: 'search',
+    component: Search,
   },
   {
     path: '/about',
     name: 'about',
     component: About,
-  },
-  {
-    path: '/search',
-    name: 'search',
-    component: Search,
   },
 ];
 
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,5 +1,44 @@
 <template>
-  <div class="search">
-    <h1>This is an search page</h1>
+  <div class="wrapper">
+      <div class="search">
+          <label for="search">Search</label>
+          <input id="search" name="search" type="text">
+      </div>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'search',
+};
+</script>
+
+<style lang="scss" scoped>
+    .wrapper {
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+        margin: 0;
+        padding: 30px;
+        width: 100%;
+
+    }
+    .search {
+        display: flex;
+        flex-direction: column;
+        width: 250px;
+
+        label {
+            text-align: left;
+        }
+
+        input {
+            height: 30px;
+            border: 0;
+            border-bottom: 2px solid #000;
+        }
+    }
+</style>
